aa92aea587c85f6985b16cdfe8879c24513092eb
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-22 23:13:15 +0300

    add login and register buttons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import SearchForm from "./SearchForm";
 
 const useStyles = makeStyles((theme) => ({
-  title: {
-    flexGrow: 1,
+  navbarWrapper: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  navbarTitle: {
     display: "none",
     textDecoration: "none",
     color: "black",
     [theme.breakpoints.up("sm")]: {
       display: "block",
+      width: "fit-content",
     },
   },
 }));
@@ -23,17 +28,27 @@ const Navigation = () => {
   return (
     <nav>
       <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography
-            className={classes.title}
-            variant="h6"
-            noWrap
-            component={Link}
-            to="/"
-          >
-            My Movie Collection
-          </Typography>
+        <Toolbar className={classes.navbarWrapper}>
+          <div>
+            <Typography
+              className={classes.navbarTitle}
+              variant="h6"
+              noWrap
+              component={Link}
+              to="/"
+            >
+              My Movie Collection
+            </Typography>
+          </div>
           <SearchForm />
+          <div>
+            <Button component={Link} to="login">
+              Login
+            </Button>
+            <Button component={Link} to="register">
+              Sign Up
+            </Button>
+          </div>
         </Toolbar>
       </AppBar>
     </nav>
