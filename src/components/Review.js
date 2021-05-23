8055378a65b6747468a9fcf982ab4d6f3257e065
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-23 23:21:37 +0300

    Update Review.js

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,34 +1,61 @@
 import { useState } from "react";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import Rating from "./Rating";
+import { addNote } from "../services/notes";
 
 const useStyles = makeStyles((theme) => ({
   reviewWrapper: {
     marginTop: "2rem",
+    marginBottom: "2rem",
   },
   reviewTextField: {
     width: "50%",
   },
+  submitButton: {
+    width: "50%",
+    marginTop: "0.5rem",
+  },
 }));
 
-const Review = () => {
+const Review = ({ movie }) => {
   const [note, setNote] = useState("");
+  const [rating, setRating] = useState("2.5");
   const classes = useStyles();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!note) return;
+    await addNote({ text: note, movieId: movie.id });
+    setNote("");
+  };
 
   return (
     <div className={classes.reviewWrapper}>
-      <Rating value="3.5" text="Your Review" />
-      <TextField
-        label="Notes"
-        multiline
-        rows={8}
-        placeholder="Your private notes and comments about the movie..."
-        variant="outlined"
-        value={note}
-        onChange={(e) => setNote(e.target.value)}
-        className={classes.reviewTextField}
-      />
+      <Rating value={rating} text="Your Review" />
+      <form onSubmit={submitHandler}>
+        <TextField
+          label="Notes"
+          multiline
+          rows={7}
+          placeholder="Your private notes and comments about the movie..."
+          variant="outlined"
+          value={note}
+          onChange={(e) => setNote(e.target.value)}
+          className={classes.reviewTextField}
+        />
+        <div>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.submitButton}
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
