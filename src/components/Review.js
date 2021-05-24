19166d0c972f117b5c65614d8d7cf54b31f5b85d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-24 19:57:28 +0300

    Update Review.js

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,28 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import Rating from "./Rating";
 import { addNote } from "../services/notes";
+import { getRating, addRating, updateRating } from "../services/ratings";
 
 const useStyles = makeStyles((theme) => ({
   reviewWrapper: {
-    marginTop: "2rem",
-    marginBottom: "2rem",
+    width: "50%",
+    margin: theme.spacing(2, 0, 2, 0),
   },
   reviewTextField: {
-    width: "50%",
+    width: "100%",
   },
   submitButton: {
-    width: "50%",
-    marginTop: "0.5rem",
+    width: "100%",
+    marginTop: theme.spacing(2),
   },
 }));
 
 const Review = ({ movie }) => {
   const [note, setNote] = useState("");
-  const [rating, setRating] = useState("2.5");
+  const [rating, setRating] = useState(0);
   const classes = useStyles();
+
+  useEffect(() => {
+    retrieveRating();
+  }, [rating]);
+
+  const retrieveRating = async () => {
+    const { data } = await getRating(movie.id);
+    if (data) {
+      setRating(data.rating);
+    }
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -31,9 +43,19 @@ const Review = ({ movie }) => {
     setNote("");
   };
 
+  const ratingHandler = async (e) => {
+    const ratingScore = Number(e.target.id || e.target.parentElement.id);
+    if (rating) {
+      await updateRating(movie.id, ratingScore);
+    } else {
+      await addRating({ rating: ratingScore, movieId: movie.id });
+    }
+    setRating(ratingScore);
+  };
+
   return (
     <div className={classes.reviewWrapper}>
-      <Rating value={rating} text="Your Review" />
+      <Rating value={rating} text="Your Review" handleClick={ratingHandler} />
       <form onSubmit={submitHandler}>
         <TextField
           label="Notes"
