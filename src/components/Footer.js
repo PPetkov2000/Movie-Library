414676ffdc37ab4cd53daacd01c7576cda4092bc
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-24 19:55:49 +0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Container from "@material-ui/core/Container";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  wrapper: {
+    marginTop: "2rem",
+    bottom: "0",
+  },
+  text: {
+    width: "100%",
+    textAlign: "center",
+  },
+}));
+
+const Footer = () => {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.wrapper}>
+      <AppBar position="static" color="default">
+        <Container>
+          <Toolbar>
+            <Typography
+              variant="body1"
+              color="inherit"
+              className={classes.text}
+            >
+              © Plamen Petkov 2021
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </footer>
+  );
+};
+
+export default Footer;
