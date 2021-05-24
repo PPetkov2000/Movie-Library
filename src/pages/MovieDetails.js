698589dc5f7b6df6f9676799be923405ea2bc244
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-24 20:01:49 +0300

    render movie review only when there is a logged in user

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,14 +1,16 @@
 import Container from "@material-ui/core/Container";
+import { useAuth } from "../contexts/AuthProvider";
 import Movie from "../components/Movie";
 import Review from "../components/Review";
 
 const MovieDetails = ({ location }) => {
   const movie = location.state;
+  const { authUser } = useAuth();
 
   return (
     <Container>
       <Movie movie={movie} />
-      <Review movie={movie} />
+      {authUser && <Review movie={movie} />}
     </Container>
   );
 };
