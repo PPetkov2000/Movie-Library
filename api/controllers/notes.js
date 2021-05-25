a4c6c98bdcb8ab304f4992ec95222c34fc0d0d77
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-25 23:05:50 +0300

    Update notes.js

--- api/controllers/notes.js
+++ api/controllers/notes.js
@@ -9,18 +9,52 @@ const getNotes = async (req, res, next) => {
   }
 };
 
-const addNote = async (req, res, next) => {
+const getNote = async (req, res, next) => {
   try {
+    const note = await Note.findById(req.params.id);
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(404);
+      throw new Error("Note not Found");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addNote = async (req, res, next) => {
+  const note = req.body.note;
+  try {
+    if (!note || typeof note !== "object" || Array.isArray(note)) {
+      res.status(400);
+      throw new Error("Please provide a valid note");
+    }
+    const createdNote = await Note.create({ ...note, user: req.user._id });
+    res.status(201).json({ note: createdNote });
   } catch (error) {
     next(error);
   }
 };
 
 const removeNote = async (req, res, next) => {
+  const noteId = req.params.id;
   try {
+    if (!noteId) {
+      res.status(400);
+      throw new Error("Please provide note id");
+    }
+    const note = await Note.findById(noteId);
+    if (note) {
+      await note.remove();
+      res.json({ message: "Note removed" });
+    } else {
+      res.status(404);
+      throw new Error("Note not Found");
+    }
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { getNotes, addNote, removeNote };
+module.exports = { getNotes, getNote, addNote, removeNote };
