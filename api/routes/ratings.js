cdb7165c67b618fe0046c09a2bc61eec29742376
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-25 23:09:05 +0300

    Update ratings.js

--- api/routes/ratings.js
+++ api/routes/ratings.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const { isAuth } = require("../middleware/auth");
 const {
+  getRatings,
   getRating,
   addRating,
+  updateRating,
   removeRating,
 } = require("../controllers/ratings");
 
-router.route("/").get(getRating).post(addRating).delete(removeRating);
+router.route("/").get(isAuth, getRatings).post(isAuth, addRating);
+router
+  .route("/:movieId")
+  .get(isAuth, getRating)
+  .put(isAuth, updateRating)
+  .delete(isAuth, removeRating);
 
 module.exports = router;
