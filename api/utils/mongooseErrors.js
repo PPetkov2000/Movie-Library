f89626ff899312155529e23227e9d4d1fff47b39
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-26 18:28:51 +0300

    Update mongooseErrors.js

--- api/utils/mongooseErrors.js
+++ api/utils/mongooseErrors.js
@@ -1,7 +1,7 @@
 module.exports = function (error) {
   const errors = Object.values(error.errors).reduce((acc, { properties }) => {
-    acc[properties.path] = properties.message;
+    acc.push(properties.message);
     return acc;
-  }, {});
+  }, []);
   return errors;
 };
