d14358f3a762162ddc93214cc32cfe5ffa01554e
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-26 18:28:38 +0300

    Update errorHandler.js

--- api/middleware/errorHandler.js
+++ api/middleware/errorHandler.js
@@ -1,3 +1,5 @@
+const mongooseErrorHandler = require("../utils/mongooseErrors");
+
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
@@ -7,6 +9,11 @@ const notFound = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   if (err.message.includes("Cast to ObjectId failed")) {
     res.status(400).json({ message: err.reason.message });
+    return;
+  }
+  if (err.message.includes("validation failed")) {
+    const errors = mongooseErrorHandler(err);
+    res.status(400).json(errors);
     return;
   }
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
