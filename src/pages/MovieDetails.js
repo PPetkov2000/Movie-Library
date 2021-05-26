258a1c153baf47d7bb26eab67a486b82e5ae7a81
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-26 18:35:46 +0300

    Update MovieDetails.js

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,11 +1,23 @@
+import { useState, useEffect } from "react";
 import Container from "@material-ui/core/Container";
 import { useAuth } from "../contexts/AuthProvider";
+import { getMovieById } from "../services/movies";
 import Movie from "../components/Movie";
 import Review from "../components/Review";
 
-const MovieDetails = ({ location }) => {
-  const movie = location.state;
+const MovieDetails = ({ match }) => {
+  const movieId = Number(match.params.id);
   const { authUser } = useAuth();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    retrieveMovie(movieId);
+  }, [movieId]);
+
+  const retrieveMovie = async (id) => {
+    const { data } = await getMovieById(id);
+    setMovie(data);
+  };
 
   return (
     <Container>
