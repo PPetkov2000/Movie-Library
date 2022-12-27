1f3be4783eebabd0e535ef08a3a59c2e5a4ed6f0
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-12-28 01:08:50 +0200

    Update api-interceptors.js

--- src/utils/api-interceptors.js
+++ src/utils/api-interceptors.js
@@ -1,35 +1,61 @@
-module.exports = function (instance) {
+const ERROR_MESSAGES_BY_STATUS = {
+  400: 'Bad request. Please provide the needed resources.',
+  401: 'You are not authenticated to access this resource.',
+  403: 'You are not authorized to access this resource.',
+  404: 'The resource you are looking for does not exist.',
+  429: 'Too many requests, please try again later.',
+  500: 'Server error, please try again later.',
+}
+
+const addInterceptors = (instance) => {
   instance.interceptors.request.use(
     (request) => {
-      if (localStorage.getItem("authUser")) {
-        request.headers.Authorization =
-          "Bearer " + JSON.parse(localStorage.getItem("authUser")).token;
+      console.log({ request })
+      request.timeout = 5000
+      if (!request.retry) request.retry = 0
+      if (localStorage.getItem('authUser')) {
+        request.headers.Authorization = 'Bearer ' + JSON.parse(localStorage.getItem('authUser')).token
+        // request.headers.Cookie = `session=${Cookies.get('session')}`
       }
-      return request;
+      return request
     },
     (error) => {
-      console.log(error);
-      return Promise.reject(error);
+      console.log({ errorRequest: error })
+      if (error.code === 'ECONNABORTED' && error.config.retry < 3) {
+        error.config.retry++
+        return instance(error.config)
+      }
+      return Promise.reject(error)
     }
-  );
+  )
 
   instance.interceptors.response.use(
     (response) => {
-      return response;
+      console.log({ response })
+      if (response.data?.newAuthToken) {
+        const authUserFromLocalStorage = JSON.parse(localStorage.getItem('authUser'))
+        const updatedAuthUser = { ...authUserFromLocalStorage, token: 'Bearer ' + response.data.newAuthToken }
+        localStorage.setItem('authUser', updatedAuthUser)
+      }
+      return response
     },
     (error) => {
-      console.log(error);
-      if (error.message === "Network Error") {
-        console.log(error);
-        return Promise.reject({ message: "Server Error" });
+      if (error.code === 'ECONNABORTED') {
+        return Promise.reject('Request took too long to complete')
       }
-      if (error.response && error.response.data) {
-        return Promise.reject(error.response.data);
+      if (error.response) {
+        const status = error?.response?.status
+        const errorMessage = Array.isArray(error)
+          ? error[0]
+          : error.response?.data?.message || ERROR_MESSAGES_BY_STATUS?.[status] || error?.message || error
+        return Promise.reject(errorMessage)
       }
-      if (error.response.status === 401) {
-        return Promise.reject({ message: "Not authorized" });
+      if (error.request) {
+        return Promise.reject('A network error occurred. Please check your internet connection and try again.')
       }
-      return Promise.reject(error.message);
+      return Promise.reject('An error occurred. Please try again.')
     }
-  );
-};
+  )
+}
+
+export default addInterceptors
