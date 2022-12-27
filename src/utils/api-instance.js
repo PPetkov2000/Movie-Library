e9608b4c414a09ffc79d16761d232abdd6578c02
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-12-28 01:07:37 +0200

    Update api-instance.js

--- src/utils/api-instance.js
+++ src/utils/api-instance.js
@@ -4,6 +4,7 @@ import addInterceptors from "./api-interceptors";
 const instance = axios.create({
   baseUrl: "http://localhost:4000",
   headers: { "Content-Type": "application/json" },
+  // withCredentials: true
 });
 
 addInterceptors(instance);
