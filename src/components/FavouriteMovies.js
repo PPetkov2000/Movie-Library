eacdf53c010b73808a35792b8ee17b3f7700f62a
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2023-07-15 23:58:06 +0300

    Update FavouriteMovies.js

--- src/components/FavouriteMovies.js
+++ src/components/FavouriteMovies.js
@@ -1,36 +1,38 @@
-import { Link } from "react-router-dom";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core/styles";
-import Loader from "./Loader";
-import Message from "./Message";
-import { defaultImage } from "../utils/defaultImage";
-import { useAuth } from "../contexts/AuthProvider";
+import { Link } from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import Loader from './Loader'
+import Message from './Message'
+import { defaultImage } from '../utils/defaultImage'
+import { useAuth } from '../contexts/AuthProvider'
 
 const useStyles = makeStyles(() => ({
   moviesWrapper: {
-    display: "grid",
-    gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-    columnGap: "1rem",
-    rowGap: "1rem",
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+    columnGap: '1rem',
+    rowGap: '1rem',
   },
   moviesTitle: {
-    textAlign: "center",
-    marginTop: "2rem",
-    marginBottom: "1.5rem",
+    textAlign: 'center',
+    marginTop: '2rem',
+    marginBottom: '1.5rem',
   },
   moviesEmpty: {
-    textAlign: "center",
+    textAlign: 'center',
   },
-}));
+}))
 
 const FavouriteMovies = () => {
-  const classes = useStyles();
-  const { loading, authUser, error } = useAuth();
+  const classes = useStyles()
+  const { loading, authUser, error } = useAuth()
 
   return (
     <Container>
-      <Typography variant="h4" className={classes.moviesTitle}>Your Favorites</Typography>
+      <Typography variant="h4" className={classes.moviesTitle}>
+        Your Favorites
+      </Typography>
       {loading ? (
         <Loader />
       ) : error ? (
@@ -39,15 +41,17 @@ const FavouriteMovies = () => {
         <div className={classes.moviesWrapper}>
           {authUser.favoriteMovies.map((movie) => (
             <Link to={`/movie/${movie.id}`} key={movie.id}>
-              <img src={movie.image ? movie.image.medium : defaultImage} alt={movie.name} />
+              <img src={movie.image ? movie.image.medium : defaultImage} alt={movie.name} loading="lazy" />
             </Link>
           ))}
         </div>
       ) : (
-        <Typography variant="h5" color="primary" className={classes.moviesEmpty}>You dont have favorite movies yet.</Typography>
+        <Typography variant="h5" color="primary" className={classes.moviesEmpty}>
+          You dont have favorite movies yet.
+        </Typography>
       )}
     </Container>
-  );
-};
+  )
+}
 
-export default FavouriteMovies;
+export default FavouriteMovies
