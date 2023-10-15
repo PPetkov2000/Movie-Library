7c6911ec3f680c665a7b5ec7dc12665e8ea9ea92
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2023-10-15 23:07:12 +0300

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,60 +1,86 @@
-import { Link } from "react-router-dom";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import { useAuth } from "../contexts/AuthProvider";
-import SearchForm from "./SearchForm";
+import { Link } from 'react-router-dom'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { useAuth } from '../contexts/AuthProvider'
+import SearchForm from './SearchForm'
+import HamburgerMenu from './HamburgerMenu'
 
 const useStyles = makeStyles((theme) => ({
   navbarWrapper: {
-    display: "flex",
-    justifyContent: "space-between",
+    display: 'flex',
+    justifyContent: 'space-between',
   },
-  navbarTitle: {
-    display: "none",
-    textDecoration: "none",
-    color: "black",
-    [theme.breakpoints.up("sm")]: {
-      display: "block",
-      width: "fit-content",
+  navbarWrapperLeftSide: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'flex',
+      alignItems: 'center',
     },
   },
-  navbarButton: {
-    fontWeight: "bold",
+  navbarWrapperRightSide: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'flex',
+      alignItems: 'center',
+    },
   },
-}));
+  navbarTitle: {
+    display: 'block',
+    width: 'fit-content',
+    textDecoration: 'none',
+    color: 'black',
+  },
+  navbarButton: {
+    fontWeight: 'bold',
+  },
+  searchFormWrapper: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+}))
 
 const Navigation = () => {
-  const classes = useStyles();
-  const { authUser, logout } = useAuth();
+  const classes = useStyles()
+  const { authUser, logout } = useAuth()
 
   return (
     <nav>
       <AppBar position="static" color="default">
         <Toolbar className={classes.navbarWrapper}>
-          <div>
-            <Typography className={classes.navbarTitle} variant="h6" noWrap component={Link} to="/">My Movie Collection</Typography>
+          <div className={classes.navbarWrapperLeftSide}>
+            <HamburgerMenu />
+            <Typography className={classes.navbarTitle} variant="h6" noWrap component={Link} to="/">
+              My Movie Collection
+            </Typography>
           </div>
-          <SearchForm />
-          <div>
+          <div className={classes.searchFormWrapper}>
+            <SearchForm />
+          </div>
+          <div className={classes.navbarWrapperRightSide}>
             {authUser ? (
               <>
                 <Button className={classes.navbarButton}>{authUser.username}</Button>
-                <Button onClick={logout} className={classes.navbarButton}>Logout</Button>
+                <Button onClick={logout} className={classes.navbarButton}>
+                  Logout
+                </Button>
               </>
             ) : (
               <>
-                <Button component={Link} className={classes.navbarButton} to="/login">Login</Button>
-                <Button component={Link} className={classes.navbarButton} to="/register">Sign Up</Button>
+                <Button component={Link} className={classes.navbarButton} to="/login">
+                  Login
+                </Button>
+                <Button component={Link} className={classes.navbarButton} to="/register">
+                  Sign Up
+                </Button>
               </>
             )}
           </div>
         </Toolbar>
       </AppBar>
     </nav>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
