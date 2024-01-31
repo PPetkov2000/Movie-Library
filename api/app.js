4c499d15765d247090735bb7dd3ffe97f132b11c
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2024-01-31 20:46:17 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -17,15 +17,15 @@ connectDB()
 
 const app = express()
 
-rateLimiter(app)
-
 process.env.NODE_ENV !== 'prod' && app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false, limit: '50mb' }))
+app.use(express.json({ limit: '10kb' }))
+app.use(express.urlencoded({ extended: false, limit: '10kb' }))
 // app.use(cors({ origin: '127.0.0.1', credentials: true }))
 app.use(cookieParser())
 app.use(setCache())
 app.use(express.static(path.join(__dirname, 'public')))
+
+rateLimiter(app)
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
